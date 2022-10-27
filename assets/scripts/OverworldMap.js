dae0aaf9d2d16957efcc674ccd816ddee26a64b0
Gwendal <ganquetil62@gmail.com>
2022-10-27 14:25:33 +0200

    bug undefined à régler

---
+++ assets/scripts/OverworldMap.js
@@ -0,0 +1,52 @@
+class OverworldMap{
+    constructor(config){
+        this.gameObjects = config.gameObjects;
+        this.lowerImage = new Image();
+        this.lowerImage.Src = config.lowerSrc;
+        this.upperImage = new Image();
+        this.upperImage.Src = config.upperSrc;
+    }
+
+    drawLowerImage(ctx){
+        ctx.drawImage(this.lowerImage,0,0)
+    }
+    drawUpperImage(ctx){
+        ctx.drawImage(this.UpperImage,0,0)
+    }
+
+}
+
+
+window.OverworldMaps = {
+    DemoRoom:{
+        lowerSrc: "img/maps/DemoLower.png",
+        upperSrc: "img/maps/DemoUpper.png",
+        hero: new gameObject({
+            x : 5,
+            y: 6,
+        }),
+        npc1: new gameObject({
+            x : 7,
+            y: 9,
+            src : "/img/characters/people/npc1.png"
+        })
+    },
+    Kitchen:{
+        lowerSrc: "img/maps/KitchenLower.png",
+        upperSrc: "img/maps/KitchenUpper.png",
+        hero: new gameObject({
+            x : 3,
+            y: 1,
+        }),
+        npc1: new gameObject({
+            x : 9,
+            y: 2,
+            src : "/img/characters/people/npc1.png"
+        }),
+        npc1: new gameObject({
+            x : 10,
+            y: 4,
+            src : "/img/characters/people/npc1.png"
+        })
+    },
+}
--- assets/scripts/overworld.js
+++ assets/scripts/overworld.js
@@ -6,33 +6,32 @@ class Overworld{
         this.element = config.element;
         this.canvas = this.element.querySelector('.game-canvas');
         this.ctx = this.canvas.getContext('2d');
+        this.map = null;
+    }
+
+
+
+    startGameLoop(){
+        const step = () => {
+
+            this.map.drawLowerImage(this.ctx);
+            Object.values(this.map.gameObjects).forEach(object =>{
+                object.sprite.draw(this.ctx)
+            })
+            this.map.drawUpperImage(this.ctx);
+            
+            requestAnimationFrame(()=>{
+                step();
+            })
+        }
+        step();
     }
 //mise en place de l'image de fond, l'environnement
 init(){
-    const image = new Image();
-    image.onload = () =>{
-        this.ctx.drawImage(image,0,0)
+this.map =  new OverworldMap(window.OverworldMaps.DemoRoom);
+this.startGameLoop();
 
-    }
 
-    image.src = "/img/maps/DemoLower.png";
-
-    //créer des GameObjects
-    const hero =  new GameObject({
-        x : 5,
-        y: 6,
-    })
-    const npc1 =  new GameObject({
-        x : 7,
-        y: 9,
-        src : "/img/characters/people/npc1.png"
-    })
-
-setTimeout(()=>{
-
-    hero.sprite.draw(this.ctx)
-    npc1.sprite.draw(this.ctx)
-},200)
 
 }
 
